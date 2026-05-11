c4816a832a1b2d75de55dc922d35db78d395eef2
hassenehadjour98@gmail.com <hassenehadjour@Hassenes-MacBook-Pro.local>
2026-05-11 19:57:57 -0400

    Fix submit page for Render

--- submit.php
+++ submit.php
@@ -1,15 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
-
-if (!isset($_SESSION['judge'])) {
-    header("Location: index.html");
-    exit();
-}
-
-include "db.php";
 
 $total = 0;
 
@@ -23,23 +13,11 @@ for ($i = 0; $i < 4; $i++) {
     }
 }
 
-$judge = $conn->real_escape_string($_POST['judge_name']);
-$group = $conn->real_escape_string($_POST['group_number']);
-$title = $conn->real_escape_string($_POST['project_title']);
-
-$sql = "INSERT INTO grades (judge_name, group_number, project_title, total)
-        VALUES ('$judge', '$group', '$title', '$total')";
-
-if (!$conn->query($sql)) {
-    die("SQL Error: " . $conn->error);
-}
-
-$result = $conn->query("SELECT ROUND(AVG(total), 2) AS avg_score 
-                        FROM grades 
-                        WHERE group_number = '$group'");
-
-$row = $result->fetch_assoc();
-$average = $row['avg_score'];
+$judge = $_POST['judge_name'];
+$group = $_POST['group_number'];
+$title = $_POST['project_title'];
+$members = $_POST['members'];
+$comments = $_POST['comments'];
 ?>
 
 <!DOCTYPE html>
@@ -51,49 +29,25 @@ $average = $row['avg_score'];
 <body>
 
 <div class="container">
-
-    <div style="text-align:right; margin-bottom:10px;">
-        Logged in as: <b><?php echo $_SESSION['judge']; ?></b>
-        <a href="logout.php"><button type="button">Logout</button></a>
-    </div>
-
     <div class="success-box">
         <h2>Grade Submitted Successfully!</h2>
 
         <table class="info-table">
-            <tr>
-                <td><b>Judge:</b></td>
-                <td><?php echo $judge; ?></td>
-            </tr>
-
-            <tr>
-                <td><b>Group Number:</b></td>
-                <td><?php echo $group; ?></td>
-            </tr>
-
-            <tr>
-                <td><b>Project Title:</b></td>
-                <td><?php echo $title; ?></td>
-            </tr>
-
-            <tr>
-                <td><b>Your Total Score:</b></td>
-                <td><b><?php echo $total; ?> / 60</b></td>
-            </tr>
-
-            <tr>
-                <td><b>Current Group Average:</b></td>
-                <td><b><?php echo $average; ?></b></td>
-            </tr>
+            <tr><td><b>Judge:</b></td><td><?php echo $judge; ?></td></tr>
+            <tr><td><b>Group Number:</b></td><td><?php echo $group; ?></td></tr>
+            <tr><td><b>Project Title:</b></td><td><?php echo $title; ?></td></tr>
+            <tr><td><b>Group Members:</b></td><td><?php echo $members; ?></td></tr>
+            <tr><td><b>Comments:</b></td><td><?php echo $comments; ?></td></tr>
+            <tr><td><b>Total Score:</b></td><td><b><?php echo $total; ?> / 60</b></td></tr>
         </table>
 
         <br>
 
         <div style="text-align:center;">
             <a href="judge.php"><button type="button">Grade Another Group</button></a>
+            <a href="logout.php"><button type="button">Logout</button></a>
         </div>
     </div>
-
 </div>
 
 </body>
