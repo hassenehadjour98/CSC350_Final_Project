0aa8e2c8fd59828b0f046fb9d177b11e3da69736
hassenehadjour98@gmail.com <hassenehadjour@Hassenes-MacBook-Pro.local>
2026-05-11 20:00:19 -0400

    Remove database connection

--- submit.php
+++ submit.php
@@ -23,31 +23,60 @@ $comments = $_POST['comments'];
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Submission Receipt</title>
-    <link rel="stylesheet" href="style.css">
+<title>Submission Receipt</title>
+<link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
 <div class="container">
-    <div class="success-box">
-        <h2>Grade Submitted Successfully!</h2>
 
-        <table class="info-table">
-            <tr><td><b>Judge:</b></td><td><?php echo $judge; ?></td></tr>
-            <tr><td><b>Group Number:</b></td><td><?php echo $group; ?></td></tr>
-            <tr><td><b>Project Title:</b></td><td><?php echo $title; ?></td></tr>
-            <tr><td><b>Group Members:</b></td><td><?php echo $members; ?></td></tr>
-            <tr><td><b>Comments:</b></td><td><?php echo $comments; ?></td></tr>
-            <tr><td><b>Total Score:</b></td><td><b><?php echo $total; ?> / 60</b></td></tr>
-        </table>
+<div class="success-box">
 
-        <br>
+<h2>Grade Submitted Successfully!</h2>
 
-        <div style="text-align:center;">
-            <a href="judge.php"><button type="button">Grade Another Group</button></a>
-            <a href="logout.php"><button type="button">Logout</button></a>
-        </div>
-    </div>
+<table class="info-table">
+
+<tr>
+<td><b>Judge:</b></td>
+<td><?php echo $judge; ?></td>
+</tr>
+
+<tr>
+<td><b>Group Number:</b></td>
+<td><?php echo $group; ?></td>
+</tr>
+
+<tr>
+<td><b>Project Title:</b></td>
+<td><?php echo $title; ?></td>
+</tr>
+
+<tr>
+<td><b>Group Members:</b></td>
+<td><?php echo $members; ?></td>
+</tr>
+
+<tr>
+<td><b>Comments:</b></td>
+<td><?php echo $comments; ?></td>
+</tr>
+
+<tr>
+<td><b>Total Score:</b></td>
+<td><b><?php echo $total; ?> / 60</b></td>
+</tr>
+
+</table>
+
+<br>
+
+<div style="text-align:center;">
+<a href="judge.php"><button type="button">Grade Another Group</button></a>
+</div>
+
+</div>
+
 </div>
 
 </body>
